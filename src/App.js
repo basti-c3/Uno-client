079125f2b3f95f1a18ab40970f58f0cf54e3685b
Basti  Kreuz <basti.kreuz@googlemail.com>
2022-04-26 13:11:23 +0200

    Change api name

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     let [socket, setSocket] = useState(null);
     let [userList, setUserList] = useState([]);
     useEffect(() => {
-        const newSocket = io(`http://${window.location.hostname}:3000`); // https://unoapi.janraab.de
+        const newSocket = io(`https://unoapi.janraab.de`); // https://unoapi.janraab.de  &&  http://${window.location.hostname}
         setSocket(newSocket);
 
         return () => newSocket.close();
